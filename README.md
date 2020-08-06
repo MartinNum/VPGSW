753fdb3b25347327a7b108e6774147b79e725805
MartinNum <1778673260@qq.com>
2020-08-06 15:26:14 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # VPGSW
 基于VP-Tree和导向小世界图（GSW）深度结合的ANNS方法
+
+## Install
+
+#### python
+
+在VPGSW目录下运行一下命令
+
+```java
+sudo python setup.py install
+```
+
+**python运行案例**：VPGSW/examples/python/example_angular.py
+
--- VPGSW/examples/python/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-time_start = time.time()
-    # for data in datas:
-    #     t.add_data(data)
-    
-    # t.build(m=20, n_threads=4)
-    # t.save('TNSWG.n10h_SIFT128d_20-20-2n_1w_L2')
-    # time_end = time.time()
